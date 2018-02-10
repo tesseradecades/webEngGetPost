748fc88289ef5eaa830b863aa08b3270cedb96e5
nwe8978 <nwe8978@g.rit.edu>
2018-02-09 22:02:13 -0500

    added files

---
+++ get_example.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<p>Full Name: <?php echo $_GET['fullName']; ?></p>
+		<p>Age: <?php echo $_GET['age']; ?></p>
+		<p>State: <?php echo $_GET['state']; ?></p>
+		<p>Gender: <?php echo $_GET['gender']; ?></p>
+	</body>
+</html>
---
+++ post_example.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<p>Full Name: <?php echo $_POST['fullName']; ?></p>
+		<p>Age: <?php echo $_POST['age']; ?></p>
+		<p>State: <?php echo $_POST['state']; ?></p>
+		<p>Gender: <?php echo $_POST['gender']; ?></p>
+	</body>
+</html>
